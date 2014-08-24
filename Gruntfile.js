af1793c3a48d2431d37c3acad58b1df9a5899b26
Joaquin <betwinsouls@gmail.com>
2014-08-24 16:22:37 -0700

    push gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-clean');
@@ -7,7 +8,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-express');
 	grunt.loadNpmTasks('grunt-open');
-	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.initConfig({
 		jshint: {
