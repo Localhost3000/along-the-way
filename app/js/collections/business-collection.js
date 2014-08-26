c92e2a80276b2f8a2166d744f6fdf5825e8b0a57
David Davidson <daviddavidson.1988@gmail.com>
2014-08-26 14:50:36 -0700

    Point the default URL at the Yelp API endpoint

--- app/js/collections/business-collection.js
+++ app/js/collections/business-collection.js
@@ -4,9 +4,16 @@ var Backbone = require('backbone');
 var $ = require('jquery');
 Backbone.$ = $;
 
+var BusinessModel = require('../models/business-model');
+
 module.exports = Backbone.Collection.extend({
-	initialize: function(models, url) {
-		this.models = models;
-		this.url = url;
+	model: BusinessModel,
+	initialize: function() {
+		this.url = 'api/0_0_1';
+	},
+	parse: function(response) {
+		// console.log(JSON.parse(response));
+		console.log(JSON.parse(response).businesses);
+		return response;
 	}
 });
