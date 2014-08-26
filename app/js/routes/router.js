37404ad3f26e12044b66f885776ddcdc9b566d71
David Davidson <daviddavidson.1988@gmail.com>
2014-08-26 11:28:24 -0700

    Moving this back to the routes dir

---
+++ app/js/routes/router.js
@@ -0,0 +1,14 @@
+var Backbone = require('backbone');
+var $ = require('jquery');
+Backbone.$ = $;
+
+module.exports = Backbone.Router.extend({
+	routes: {
+		'': 'init',
+		'map': 'map',
+		'destinations': 'businessCollection'
+	},
+	init: require('./init'),
+	map: require('./map'),
+	businessCollection: require('./business-collection')
+});
