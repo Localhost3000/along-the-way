aea27198af67040f48ef39f1b2c5aae685bd49ef
David Davidson <daviddavidson.1988@gmail.com>
2014-08-27 15:37:45 -0700

    Get rid of unnecessary console.log()ing

--- app/js/views/init-view.js
+++ app/js/views/init-view.js
@@ -28,16 +28,27 @@ var InitView = Backbone.View.extend({
 		var start = this.$el.closest('div').find('#start').val();
 		var destination = this.$el.closest('div').find('#destination').val();
 
-		// Pass data into the model (which is owned by the router)
+		// Pass data into the model (which is owned by the router, and globally visible)
 		this.model.set('start', start);
 		this.model.set('end', destination);
 		console.log('After change, in the model: ' + this.model.get('start'));
 
 		// To do: add Google API call that gets route midpoints
-		// Pass them into new BusinessCollection(), instead of start
 		var BusinessCollection = require('../collections/business-collection');
-		var testCollection = new BusinessCollection(start, {});
-		testCollection.fetch(); // Populates businesses asynchronously
+
+		// Note that "start" is now unnecessary here; we might still want to pass in
+		// the options object on init, though:
+		var businessCollection = new BusinessCollection(start, {});
+
+		// This replaces businessCollection.fetch() for now
+		businessCollection.search(
+		[
+			{"lat":47.6228484,"lng":-122.335816},
+			{"lat":47.622604,"lng":-122.3371348},
+			{"lat":47.6185347,"lng":-122.3371732},
+			{"lat":47.6185383,"lng":-122.3384741},
+			{"lat":47.61834,"lng":-122.3384572}
+		]);
 
 		// Finally, hit up the next view:
 		Backbone.history.navigate('#map', {
