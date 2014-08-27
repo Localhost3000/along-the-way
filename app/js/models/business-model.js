3b7a332665faadb1d1a82d90fd3ddde44ca5c983
David Davidson <daviddavidson.1988@gmail.com>
2014-08-27 16:28:47 -0700

    Test console.log, and tweak indentation

--- app/js/models/business-model.js
+++ app/js/models/business-model.js
@@ -3,10 +3,10 @@
 var Backbone = require('backbone');
 var BusinessModel = Backbone.Model.extend({
 	parse: function(data) {
-        this.name = data.name;
-        this.address = data.location.display_address.join(' ');
-        this.rating = data.rating;
-        this.specificCategory = data.categories[0][0];
+    this.name = data.name;
+    this.address = data.location.display_address.join(' ');
+    this.rating = data.rating;
+    this.specificCategory = data.categories[0][0];
 	}
 });
 module.exports = BusinessModel;
