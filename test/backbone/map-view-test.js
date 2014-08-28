8dabc10470d65708f1ebb54a3c2fbbbb4c3ffe75
Brock Beaudry <brock.beaudry@gmail.com>
2014-08-28 10:34:42 -0700

    Initial map view unit tests

---
+++ test/backbone/map-view-test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/* jshint expr: true */
+
+var chai = require('chai');
+var sinon = require('sinon');
+var expect = chai.expect;
+var Backbone = require('backbone');
+
+var MapView = require('../../app/js/views/map-view');
+var MapModel = require('../../app/js/models/map-model');
+var BusinessCollection = require('../../app/js/collections/business-collection');
+
+describe('Backbone Map View', function() {
+  before(function(done) {
+    sinon.spy(MapView.prototype, 'render');
+    sinon.spy(MapView.prototype, 'getDirections');
+
+    this.collection = new BusinessCollection();
+    this.mapModel = new MapModel();
+    this.mapView = new MapView({ model: this.mapModel, businesses: this.collection} );
+    done();
+  });
+
+  it('should call render on creation', function(done) {
+    expect(MapView.prototype.render.called).to.be.true;
+    done();
+  });
+
+  it('should call getDirections', function(done) {
+    expect(MapView.prototype.getDirections.called).to.be.true;
+    done();
+  });
+
+  it('should not have a blank $el', function(done) {
+    expect(this.mapView.el).to.not.eql('');
+    done();
+  });
+
+  after(function(done) {
+    MapView.prototype.render.restore();
+    MapView.prototype.getDirections.restore();
+    done();
+  });
+});
