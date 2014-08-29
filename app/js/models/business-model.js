2580693b12fc9d376919cdbc6d2225c43bd8ec41
David Davidson <daviddavidson.1988@gmail.com>
2014-08-29 12:08:09 -0700

    Remove gross comments

--- app/js/models/business-model.js
+++ app/js/models/business-model.js
@@ -4,18 +4,26 @@ var Backbone = require('backbone');
 var BusinessModel = Backbone.Model.extend({
 	idAttribute: 'id',
 	parse: function(data) {
-        console.log('data coming into model: ' +
-            data.name +
-            ' | ' +
-            data.location.display_address +
-            ' | distance: ' +
-            data.distance);
+        // console.log('data coming into model: ' +
+        //     data.name +
+        //     ' | ' +
+        //     data.location.display_address +
+        //     ' | distance: ' +
+        //     data.distance);
 
         var hash = {};
         hash.name = data.name;
         hash.id = data.id;
         hash.address = data.location.display_address.join(' ');
         hash.rating = data.rating;
+
+        if (data.location.coordinate && data.location.coordinate !== 'undefined') {
+            // console.log('coordinates in model!');
+            hash.coordinates = {
+                lat: data.location.coordinate.latitude,
+                lng: data.location.coordinate.longitude
+            };
+        }
 
         if (data.categories && data.categories !== 'undefined') {
         	hash.specificCategory = data.categories[0][0];
