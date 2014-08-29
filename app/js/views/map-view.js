8bcebed707aed732b77c2f21cd1ad4de6c05b01a
David Davidson <daviddavidson.1988@gmail.com>
2014-08-29 15:32:20 -0700

    snapshot

--- app/js/views/map-view.js
+++ app/js/views/map-view.js
@@ -51,50 +51,105 @@ module.exports = Backbone.View.extend({
     var self = this;
     var geocoder = new google.maps.Geocoder();
     var i = 0,
-    delay = 100,
+    delay = 200,
     successCounter = 0;
 
-    var addMarker = function(position, name) {
-      var marker = new google.maps.Marker({
-        map: self.map,
-        position: position,
-        title: name
-      });
-    };
-
     function recurse() {
-      var highlight = self.businesses.models[i].get('name');
+      console.log('i: ' + i);
+      console.log('Current speed: ' + delay);
 
-      if (self.businesses.models[i].attributes.coordinates) {
+      /**
+       * Trial version
+       *==============
+       *
+       *
+       * Comment it back in (and the traditional one out) if you want to compare!
+       *
+       * This version attempts to regulate its own timeout intervals
+       *
+       * Otherwise, we need to run at, like, 400 to 600 ms minimum :(
+       */
 
-        // Yelp available
-        addMarker(self.businesses.models[i].attributes.coordinates, highlight);
-        if (i++ < self.businesses.length - 1) {
-          setTimeout(recurse, delay);
-        }
-      } else {
 
-        // Geocoder :(
-        geocoder.geocode({
-          'address': self.businesses.models[i].attributes.address
-        }, function(results, status) {
-          try {
-            if (status === google.maps.GeocoderStatus.OK) {
-              addMarker(results[0].geometry.location, highlight);
-              if (i++ < self.businesses.length - 1) {
-                setTimeout(recurse, delay);
+      geocoder.geocode({
+        'address': self.businesses.models[i].attributes.address
+      }, function(results, status) {
+
+        // Move the Geocoder attempt into a try/catch block
+        try {
+
+          if (status === google.maps.GeocoderStatus.OK) {
+            console.log('success!');
+            successCounter++;
+            var marker = new google.maps.Marker({
+              map: self.map,
+              position: results[0].geometry.location
+            });
+
+            // If we're successful, increment i and call recurse again
+            if (i++ < self.businesses.length - 1) {
+              // Successful more than 3 times in a row? Speed up a little
+              if (successCounter > 3) {
+                delay = (delay > 50 ? delay - 50 : 0);
               }
-            } else {
-              throw status;
+              setTimeout(recurse, delay); // Call self
             }
+
+          // Error from Google? Throw an exception...
+          } else {
+            throw status;
           }
-          catch(err) {
-            // console.log(err);
-            setTimeout(recurse, delay);
-          }
-        });
-      }
+        }
+
+        // ... and catch it here
+        catch(err) {
+          console.log(err);
+          successCounter = 0; // duh
+          delay = delay + 100; // Slow down execution (twice as aggressively as we ever speed it up)
+          setTimeout(recurse, delay); // Call self
+        }
+      });
     }
+
+
+    /**
+     * End self-regulating version
+     */
+
+
+    /**
+     * Begin normal version
+     */
+
+    //   delay = 500; // <= Play with this
+
+    //   geocoder.geocode({
+    //     'address': self.businesses.models[i].attributes.address
+    //   }, function(results, status) {
+    //     try {
+    //       if (status === google.maps.GeocoderStatus.OK) {
+    //         console.log('success!');
+    //         var marker = new google.maps.Marker({
+    //           map: self.map,
+    //           position: results[0].geometry.location
+    //         });
+    //         if (i++ < self.businesses.length - 1) {
+    //           setTimeout(recurse, delay);
+    //         }
+    //       } else {
+    //         throw status;
+    //       }
+    //     }
+    //     catch(err) {
+    //       console.log(err);
+    //       setTimeout(recurse, delay);
+    //     }
+    //   });
+    // }
+
+    /**
+     * End old-fashioned version
+     */
 
     recurse(); // Kick off the function for the first time
     this.render();
@@ -102,7 +157,7 @@ module.exports = Backbone.View.extend({
   },
 
   render: function() {
-    $('#map-canvas').replaceWith(this.$el);
+    $('#backbone').replaceWith(this.$el);
     return this;
   }
 });
