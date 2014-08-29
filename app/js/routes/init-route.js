8b2a2cfebcdc651909033a9418627d9f078f2141
David Davidson <daviddavidson.1988@gmail.com>
2014-08-28 20:40:01 -0700

    Initialize business collection with a radius of 500

--- app/js/routes/init-route.js
+++ app/js/routes/init-route.js
@@ -1,21 +1,18 @@
 'use strict';
-// update
 
 var Backbone = require('backbone');
 var $ = require('jquery');
 Backbone.$ = $;
 
 var BusinessCollection = require('../collections/business-collection');
-
-// Test: require in a route model?
 var MapModel = require('../models/map-model');
 
 module.exports = function() {
-	// test: bind an instance to the view
 	this.mapModel = new MapModel();
-	this.collection = new BusinessCollection();
+	this.collection = new BusinessCollection({
+		radius_filter: 500
+	});
 
-	// end test
 	var InitView = require('../views/init-view');
 	var initView = new InitView({model: this.mapModel, collection: this.collection});
 	$('#backbone').html(initView.$el);
