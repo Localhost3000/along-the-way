68b6e3bce51ebf9ceb3791508d4317faf80389a8
David Davidson <daviddavidson.1988@gmail.com>
2014-08-29 12:33:19 -0700

    Wider border radius

--- app/js/routes/init-route.js
+++ app/js/routes/init-route.js
@@ -10,7 +10,7 @@ var MapModel = require('../models/map-model');
 module.exports = function() {
 	this.mapModel = new MapModel();
 	this.collection = new BusinessCollection({
-		radius_filter: 20
+		radius_filter: 100
 	});
 
 	var InitView = require('../views/init-view');
