5b585265608be77a91d1033f6796d1e9de7165f6
David Davidson <daviddavidson.1988@gmail.com>
2014-08-29 09:40:19 -0700

    Add a few more tests for our Yelp endpoint

--- test/mocha/api/express-routes-test.js
+++ test/mocha/api/express-routes-test.js
@@ -6,18 +6,34 @@ var expect = require('chai').expect;
 
 chai.use(chaihttp);
 
-require('../../../server');
-
+var server = require('../../../server');
 
 describe('Yelp search route', function() {
-
+  console.log('Should look on port ' + server.port);
   it('should be able to get', function(done) {
-    chai.request('http://localhost:3000')
-      .get('/api/0_0_1/location' )
-      .res(function(res) {
-        expect(res).to.have.status(200);
-      });
+    chai.request('http://localhost:' + server.port)
+    .get('/api/0_0_1/[{"lat":47.6613676,"lon":-122.32989020000002}]/{}')
+    .res(function(res) {
+	      expect(res).to.have.status(200);
       done();
+    });
   });
 
+  it('should return an array', function(done) {
+  	chai.request('http://localhost:' + server.port)
+    .get('/api/0_0_1/[{"lat":47.6613676,"lon":-122.32989020000002}]/{}')
+    .res(function(res) {
+	      expect(Array.isArray(res.body)).to.be.true;
+      done();
+    });
+  });
+
+  it('should return Yelp objects within the array', function(done) {
+  	chai.request('http://localhost:' + server.port)
+    .get('/api/0_0_1/[{"lat":47.6613676,"lon":-122.32989020000002}]/{}')
+    .res(function(res) {
+	      expect(JSON.parse(res.body[0])).to.have.property('businesses');
+      done();
+    });
+  });
 });
